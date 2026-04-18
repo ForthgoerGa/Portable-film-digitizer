ca2a14bd00ad0b59a6bc137ddbc1d20dfc52217c
Arnav Gaddam <108426505+arnavgaddam@users.noreply.github.com>
2026-04-18 18:48:11 -0500

    Serial command interface

--- firmware/film_digitizer/Core/Src/main.c
+++ firmware/film_digitizer/Core/Src/main.c
@@ -2,17 +2,7 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  * @brief          : Main program body with UART Motor Control and Panic Stop
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -21,44 +11,26 @@
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
 void DWT_Init(void);
 void delay_us(uint32_t us);
+void Process_Command(char* buffer);
 /* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+uint8_t rx_byte;                // Single byte received from UART
+char rx_buffer[64];             // Buffer to store the incoming string
+uint8_t rx_index = 0;           // Current position in buffer
+volatile uint8_t cmd_ready = 0; // Flag set when ';' is received
+volatile uint8_t stop_flag = 0; // Emergency stop flag
+/* USER CODE END PV */
 
 /**
   * @brief  The application entry point.
@@ -66,78 +38,145 @@ void delay_us(uint32_t us);
   */
 int main(void)
 {
-
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM3_Init();
+
   /* USER CODE BEGIN 2 */
   DWT_Init();
-  uint32_t speed_delay = 800;
-  int32_t target_delay = 50;
 
+  // Start the UART Interrupt to listen for the first byte
+  HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
+
+  // Optional: Boot message to confirm serial is working
+  char boot_msg[] = "Motor Controller Ready. Press 'q' to panic stop.\r\n";
+  HAL_UART_Transmit(&huart2, (uint8_t*)boot_msg, strlen(boot_msg), 100);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
   while (1)
   {
-	// Step the motor
-	HAL_GPIO_WritePin(GPIOA, STEP_X_PIN_Pin, GPIO_PIN_SET);
-	delay_us(speed_delay);
-	HAL_GPIO_WritePin(GPIOA, STEP_X_PIN_Pin, GPIO_PIN_RESET);
-	delay_us(speed_delay);
+    if (cmd_ready)
+    {
+      Process_Command(rx_buffer);
+      cmd_ready = 0; // Reset flag after processing
+    }
+    /* USER CODE END WHILE */
 
-	// Simple Acceleration Ramp
-	if (speed_delay > target_delay) {
-		speed_delay--; // Decrease delay to increase speed
-	}
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
 
-	// Optional: Blink LED every 500 steps to show life
-	static uint32_t step_count = 0;
-	if (++step_count > 500) {
-		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-		step_count = 0;
-	}
+/**
+  * @brief UART Receive Callback - Triggered every time a byte arrives
+  */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  if (huart->Instance == USART2)
+  {
+    // --- PANIC BUTTON CHECK ---
+    if (rx_byte == 'q')
+    {
+      stop_flag = 1;
+      char panic_alert[] = "\r\n!!! PANIC STOP TRIGGERED !!!\r\n";
+      HAL_UART_Transmit(huart, (uint8_t*)panic_alert, strlen(panic_alert), 50);
+    }
 
+    // --- DEBUG ECHO ---
+    HAL_UART_Transmit(huart, &rx_byte, 1, 10);
+
+    // --- PROTOCOL PARSING ---
+    if (rx_byte == ';') // End of command
+    {
+      rx_buffer[rx_index] = '\0'; // Null terminate string
+      cmd_ready = 1;
+      rx_index = 0;
+    }
+    else if (rx_byte == '$') // Start of command
+    {
+      rx_index = 0;
+    }
+    else if (rx_index < 63)
+    {
+      rx_buffer[rx_index++] = rx_byte;
+    }
+
+    // Restart interrupt to receive next byte
+    HAL_UART_Receive_IT(&huart2, &rx_byte, 1);
   }
 }
 
+/**
+  * @brief Logic to parse and execute commands
+  */
+void Process_Command(char* buffer)
+{
+  // Send an acknowledgment back to the terminal
+  char msg[64];
+  sprintf(msg, "Executing: %s\r\n", buffer);
+  HAL_UART_Transmit(&huart2, (uint8_t*)msg, strlen(msg), 100);
 
+  // Format expected: "MX:1000" (Move X)
+  if (strncmp(buffer, "MX:", 3) == 0)
+  {
+    int32_t steps = atoi(&buffer[3]);
+    uint32_t speed_delay = 800;  // Start slow for every move
+    uint32_t target_delay = 100; // Final speed
+
+    stop_flag = 0; // Reset stop flag before starting move
+
+    // Set Direction
+    if (steps > 0) {
+      HAL_GPIO_WritePin(DIR_X_PIN_GPIO_Port, DIR_X_PIN_Pin, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(DIR_X_PIN_GPIO_Port, DIR_X_PIN_Pin, GPIO_PIN_RESET);
+      steps = -steps; // Make steps positive for the loop
+    }
+
+    // Move loop with Ramp and Panic Check
+    for (int32_t i = 0; i < steps; i++)
+    {
+      // Check if 'q' was pressed during the move
+      if (stop_flag)
+      {
+        char stop_ack[] = "Motor Aborted.\r\n";
+        HAL_UART_Transmit(&huart2, (uint8_t*)stop_ack, strlen(stop_ack), 50);
+        break;
+      }
+
+      HAL_GPIO_WritePin(STEP_X_PIN_GPIO_Port, STEP_X_PIN_Pin, GPIO_PIN_SET);
+      delay_us(speed_delay);
+      HAL_GPIO_WritePin(STEP_X_PIN_GPIO_Port, STEP_X_PIN_Pin, GPIO_PIN_RESET);
+      delay_us(speed_delay);
+
+      // Simple ramp down of delay (increase speed)
+      if (speed_delay > target_delay && i % 2 == 0) {
+        speed_delay--;
+      }
+    }
+
+    stop_flag = 0; // Ensure flag is clear for next command
+  }
+}
 
 /**
   * @brief System Clock Configuration
-  * @retval None
   */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -147,63 +186,17 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 336;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
   RCC_OscInitStruct.PLL.PLLQ = 7;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
-
-/* Initialize DWT for microsecond delays */
 void DWT_Init(void) {
   CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
   DWT->CYCCNT = 0;
@@ -214,4 +207,9 @@ void delay_us(uint32_t us) {
   uint32_t startTick = DWT->CYCCNT;
   uint32_t delayTicks = us * (SystemCoreClock / 1000000);
   while (DWT->CYCCNT - startTick < delayTicks);
+}
+
+void Error_Handler(void) {
+  __disable_irq();
+  while (1) {}
 }
